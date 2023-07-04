3cf97e254f61408ca88499da3fb203c3523e47c2
danilu <vorobev@danilu.ru>
2023-07-05 00:08:58 +0300

    add ru language

--- todolist/settings.py
+++ todolist/settings.py
@@ -74,7 +74,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
