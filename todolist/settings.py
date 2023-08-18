b9aeb305acdaf9b2ed49cfa03286c4eb2996fe95
danilu <danilu2537@gmail.com>
2023-08-19 00:37:30 +0300

    github actions: add ruff and pytest check

--- todolist/settings.py
+++ todolist/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': env.str('DB_ENGINE'), 
+        'ENGINE': 'django.db.backends.postgresql', 
         'NAME': env.str('POSTGRES_DB'),
         'USER': env.str('POSTGRES_USER'),
         'PASSWORD': env.str('POSTGRES_PASSWORD'),
